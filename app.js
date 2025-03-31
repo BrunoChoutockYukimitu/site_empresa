8b45adbf85d050813463642db412bc5aebb8258a
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-03-31 19:15:31 -0300

    refatoração de código

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-console.log("Bruno")
-console.log(17)
-console.warn("atenção")
-console.error("um erro ocorreu")
-/*
-var
-let essas são 3 modos de montar uma variável
-const
-*/
-
-let nome = "Bruno"
-let idade = "17"
-let telefone = null
-console.log(nome, idade)
-nome = "Bruno Choutock"
-console.log(nome)
-
 let nomeCompleto = prompt ("informe seu nome completo")
 console.log(nomeCompleto)
 /*alert("Seja bem vindo(a)" + nomeCompleto)*/
