5a52262ce4fb48bed459a384e595fad75e932c77
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-03-31 19:50:46 -0300

    exercicio 01

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let nomeCompleto = prompt ("informe seu nome completo")
 console.log(nomeCompleto)
-/*alert("Seja bem vindo(a)" + nomeCompleto)*/
 
-/*document.write("Seja bem vindo(a)" + nomeCompleto)*/
+function fnTrocarDeCorDeFundo(){
 let cor = prompt ("Informe uma cor:\n 1 azul \n 2 vermelno \n 3 preto")
 let corDeFundo = null
 if ( cor =="1"){
@@ -12,10 +11,13 @@ else if (cor == "2"){
     corDeFundo = "red"
 }
 else if (cor == "3"){
-    corDeFundop ="black"
+    corDeFundo ="black"
 }
 else {
     corDeFundo = "yellow"
 }
 
 document.body.style.backgroundColor = corDeFundo
+}
+
+fnTrocarDeCorDeFundo()
---
+++ exercicio-01/ex01.js
@@ -0,0 +1,4 @@
+let numero1 = prompt ("Digite o primeiro número")
+let numero2 = prompt ("Digite se segundo número")
+let resultado = parseFloat(numero1) + parseFloat(numero2)
+alert("o resultado é :" +   resultado)
