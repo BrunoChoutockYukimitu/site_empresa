2a3ccfe14de1c919a806421248197fd1680c0754
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-03-31 22:00:59 -0300

    cotação em dólar

---
+++ exercicio-10/ex10.js
@@ -0,0 +1,4 @@
+let cotacao = prompt("Informe a cotação do dólar")
+let valorEmDolar =prompt("Informe o valor em sólares que você tem")
+let valoreEmReais = cotacao * valorEmDolar
+alert("Você possui R$" + valoreEmReais)
